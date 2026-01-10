d61bde59f15a2a53047f7cf3e259e5d4eb246eb7
Andrian Indyanto <indyanto07@gmail.com>
2026-01-10 19:15:48 +0700

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,25 @@
+variabel_list = [1,'haii',True,1.0]
+variabel_list[0]='dicoding'
+print(variabel_list)
+x1={1,2,3,4,5}
+x2={4,5,6,7,8,9,10}
+
+union = x1.union(x2)
+print(union)
+
+intersection = x1.intersection(x2)
+print(intersection)
+
+x = {'name':'andrew','age':'18','status':'single'}
+x['job'] = "Data Scientist"
+x['name'] ="acen"
+del x['status'] 
+print(x)
+# print(x['name'])
+# print(x['age'])
+# print(x['status'])
+print(list("andre"))
+print(dict([[1,3],[2,4]]))
+
+data_diri = {'firstName' : 'Andrian','lastName' : 'Indyanto','age' : 19,'isMarried' : False}
+print(data_diri['firstName'])
