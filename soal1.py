f9be4f70347632c1cbf2fe9dda47c8bfc2de8f6a
Andrian Indyanto <indyanto07@gmail.com>
2026-01-31 23:39:17 +0700

    Add files via upload

--- soal1.py
+++ soal1.py
@@ -1,12 +1,8 @@
 # Code Latihan 1 di sini
 data_kotor = [100, 500, 100, 200, 300, 500, 200]
 
-satu = list(set(data_kotor))
-print(f"Data Bersih: {satu}")
-
-satu.sort(reverse=True)
-print(f"Data Terbesar ke Terkecil: {satu}")
-
-satu.append(999)
-satu.sort(reverse=True)
-print(f"Ubah data di urutan pertama: {satu}")
+proses = list(set(data_kotor))
+proses.sort(reverse=True)
+proses.append(999)
+proses.sort(reverse=True)
+print(f"Data setelah diproses: {proses}")
